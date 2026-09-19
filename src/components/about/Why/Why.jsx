00073fbdcb174c0ec2e2fef5e6f86b0697feebce
Archit Patle <architpatle06@gmail.com>
2026-09-19 22:17:09 +0530

    Added Why Section in About Page

---
+++ src/components/about/Why/Why.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './Why.module.css'
+
+import Eyebrow from '../../ui/Eyebrow/Eyebrow'
+
+import whyImg from '../../../assets/images/about/why.png'
+
+const Why = () => {
+    return (
+        <section className={`${styles.why} section-sm `}>
+
+            <div className={`${styles.contentWrapper} container `}>
+
+                {/* Eyebrow  */}
+                <Eyebrow eyebrowSub="Why" eyebrowMain="was SkillBridge created?" />
+
+                <h2 className={`${styles.headingExtra} heading `} >Career decisions shouldn't be based on guesswork.</h2>
+
+                <div className={`${styles.content}`}>
+                    <p className={`${styles.descriptionExtra} description `} >Students and freshers often know where they want to go, but not what it takes to get there. SkillBridge was created to bridge that gap by turning skills, role requirements, and career goals into clear, actionable direction.
+                        <br /><br />
+
+                        Choosing a career path can feel overwhelming when you don't know which skills matter or where your current abilities stand. SkillBridge brings these pieces together to help you understand your position and make more informed career decisions.
+                        <br /><br />
+                        Learning without a clear direction can lead to wasted time, scattered efforts, and uncertainty about what comes next. SkillBridge helps turn that uncertainty into a focused path by showing you what to improve and where to go from there.
+                    </p>
+                    <div className={`${styles.imageWrapper}`}>
+                        <img src={whyImg} alt="whyImg" />
+                    </div>
+                </div>
+
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Why
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/sections/Hero/Hero'
 import heroData from '../data/heroData'
+import Why from '../components/about/Why/Why'
 
 const About = () => {
   return (
     <>
       <Hero data ={heroData.about} />
+      <Why />
     </>
   )
 }
